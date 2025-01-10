cc52ba147e77168cbc12530b9a5349d46447bd99
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2025-01-10 09:28:31 +0900

    feat: User 애거리거트 구현 완료
    mod: UserRepository 저장 메서드 이름 변경 -> saveUser()

--- src/main/java/com/hhp7/concertreservation/domain/user/repository/UserRepository.java
+++ src/main/java/com/hhp7/concertreservation/domain/user/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.hhp7.concertreservation.domain.user.repository;
 
+import com.hhp7.concertreservation.domain.user.model.User;
+import java.util.Optional;
+
 public interface UserRepository {
+
+    // 회원 조회
+    Optional<User> findUserByUserId(String userId);
+
+    // 회원 가입
+    User saveUser(User user);
+
 }
