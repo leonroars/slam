976d669b79292fb4006212d699bf77e162ad1bbd
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2025-01-10 09:28:31 +0900

    feat: User 애거리거트 구현 완료

--- src/main/java/com/hhp7/concertreservation/domain/user/model/User.java
+++ src/main/java/com/hhp7/concertreservation/domain/user/model/User.java
@@ -1,4 +1,39 @@
 package com.hhp7.concertreservation.domain.user.model;
 
+import com.hhp7.concertreservation.domain.point.model.Point;
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
 public class User {
+    private final String id;
+    private final String name;
+
+    // 기본 생성자. : Private
+    private User(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public static User create(String id, String name){
+        return new User(id, name);
+    }
+
+    // 실제 생성 시에는 사용자의 이름만 받아 생성하게 되므로 다음과 같은 팩토리 메서드를 정의한다.
+    public static User create(String name){
+        return new User(
+                String.valueOf(UUID.randomUUID())
+                , name
+        );
+    }
+
+    // Domain Model Entity 간 비교를 위해 equals() 구현.
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User user) {
+            return this.id.equals(user.id);
+        }
+        return false;
+    }
 }
--- src/main/java/com/hhp7/concertreservation/domain/user/service/UserService.java
+++ src/main/java/com/hhp7/concertreservation/domain/user/service/UserService.java
@@ -1,4 +1,25 @@
 package com.hhp7.concertreservation.domain.user.service;
 
+import com.hhp7.concertreservation.domain.user.model.User;
+import com.hhp7.concertreservation.domain.user.repository.UserRepository;
+import com.hhp7.concertreservation.exceptions.UnavailableRequestException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    // 회원 가입
+    public User joinUser(User user) {
+        return userRepository.joinUser(user);
+    }
+
+    // 회원 조회
+    public User findUserByUserId(String userId) {
+        return userRepository.findUserByUserId(userId)
+                .orElseThrow(() -> new UnavailableRequestException("해당 사용자가 존재하지 않으므로 조회가 불가합니다."));
+    }
+
 }
