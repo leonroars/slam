65da7bd4e7bcbd0a0fc377235bba2796604e740f
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2025-02-06 21:31:17 +0900

    feat: Redisson 기반 분산 락 패키지 위치 조정

--- src/main/java/com/hhp7/concertreservation/infrastructure/distributedlock/redisson/config/RedissonConfig.java
+++ src/main/java/com/hhp7/concertreservation/infrastructure/distributedlock/redisson/config/RedissonConfig.java
@@ -1,4 +1,4 @@
-package com.hhp7.concertreservation.infrastructure.redis.redisson.config;
+package com.hhp7.concertreservation.infrastructure.distributedlock.redisson.config;
 
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
