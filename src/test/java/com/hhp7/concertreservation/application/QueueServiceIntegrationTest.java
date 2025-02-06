2bcd11dd1de508f36471cbd032bb86d6ca367b65
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2025-02-07 06:10:51 +0900

    feat: QueueService - 통합테스트(동시성/비동시성) 작성 완료

--- src/test/java/com/hhp7/concertreservation/application/QueueServiceIntegrationTest.java
+++ src/test/java/com/hhp7/concertreservation/application/QueueServiceIntegrationTest.java
@@ -2,10 +2,23 @@ package com.hhp7.concertreservation.application;
 
 import com.hhp7.concertreservation.domain.concert.model.ConcertSchedule;
 import com.hhp7.concertreservation.domain.concert.service.ConcertService;
+import com.hhp7.concertreservation.domain.queue.model.Token;
 import com.hhp7.concertreservation.domain.queue.service.QueueService;
 import com.hhp7.concertreservation.domain.user.model.User;
 import com.hhp7.concertreservation.domain.user.service.UserService;
+import com.hhp7.concertreservation.exceptions.BusinessRuleViolationException;
+import com.hhp7.concertreservation.exceptions.UnavailableRequestException;
 import java.time.LocalDateTime;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -34,11 +47,6 @@ public class QueueServiceIntegrationTest {
     @Nested
     class NonConcurrentScenario {
 
-        @AfterEach
-        void tearDown() {
-
-        }
-
         @Test
         @DisplayName("성공 : 어떤 사용자가 대기열에 진입하면 대기열 토큰이 발급된다.")
         void shouldSuccessIssueToken_WhenEnterQueue() {
@@ -47,89 +55,177 @@ public class QueueServiceIntegrationTest {
             ConcertSchedule concertSchedule
                     = ConcertSchedule.create(CONCERT_SCHEDULE_ID, CONCERT_SCHEDULE_START_TIME, CONCERT_SCHEDULE_RESERVATION_START_TIME, CONCERT_SCHEDULE_RESERVATION_END_TIME);
 
+            User savedUser = userService.joinUser(user);
+            ConcertSchedule savedConcertSchedule = concertService.registerConcertSchedule(concertSchedule, 10000);
+
             // when : 해당 사용자가 해당 공연 일정 대기열 진입
+            Token expectedToken = queueService.issueToken(savedUser.getId(), savedConcertSchedule.getId());
 
-            // then : 대기열 토큰이 발급된다.
-
+            // then : 발급된 토큰이 존재한다. 해당 공연일정과 Token ID로 조회시 '대기 상태' 토큰이 존재한다.
+            Assertions.assertThatThrownBy(() -> queueService.validateToken(savedConcertSchedule.getId(), expectedToken.getId()))
+                    .isInstanceOf(UnavailableRequestException.class)
+                    .hasMessageContaining("대기 중인 토큰은 사용 불가합니다.");
         }
 
         @Test
         @DisplayName("성공 : 한 번에 K개의 대기열 토큰 상태를 '활성화'로 변경할 수 있다.")
         void shouldSuccessActivateKTokens_WhenActivateKTokensAtOnce() {
-            // given : K개의 대기열 토큰이 존재하고, 모두 '대기' 상태.
+            // given : 2개의 대기열 토큰이 존재하고, 모두 '대기' 상태.
+            User user1 = User.create("user1");
+            User user2 = User.create("user2");
 
-            // when : K개의 대기열 토큰 상태를 '활성화'로 변경
+            ConcertSchedule concertSchedule
+                    = ConcertSchedule.create(CONCERT_SCHEDULE_ID, CONCERT_SCHEDULE_START_TIME, CONCERT_SCHEDULE_RESERVATION_START_TIME, CONCERT_SCHEDULE_RESERVATION_END_TIME);
+
+            User savedUser1 = userService.joinUser(user1);
+            User savedUser2 = userService.joinUser(user2);
+            ConcertSchedule savedConcertSchedule = concertService.registerConcertSchedule(concertSchedule, 10000);
+
+            // 토큰 2개 발급 / 대기 상태
+            Token token1 = queueService.issueToken(savedUser1.getId(), savedConcertSchedule.getId());
+            Token token2 = queueService.issueToken(savedUser2.getId(), savedConcertSchedule.getId());
+
+            // when : 2개의 대기열 토큰 상태를 '활성화'로 변경
+            queueService.activateTokens(savedConcertSchedule.getId(), 2);
 
             // then : K개의 대기열 토큰 상태가 '활성화'로 변경된다.
+            Assertions.assertThat(queueService.validateToken(savedConcertSchedule.getId(), token1.getId()))
+                    .isEqualTo(true);
+            Assertions.assertThat(queueService.validateToken(savedConcertSchedule.getId(), token2.getId()))
+                    .isEqualTo(true);
         }
 
         @Test
         @DisplayName("성공 : 한 번에 K 개의 대기열 토큰을 만료 시킬 수 있다.")
         void shouldSuccessExpireKTokens_WhenExpireKTokensAtOnce() {
-            // given : K개의 대기열 토큰이 존재하고, 모두 '활성화' 상태.
+            // given : 2개의 대기열 토큰이 존재하고, 모두 '활성화' 상태.
+            User user1 = User.create("user1");
+            User user2 = User.create("user2");
+            ConcertSchedule concertSchedule
+                    = ConcertSchedule.create(CONCERT_SCHEDULE_ID, CONCERT_SCHEDULE_START_TIME, CONCERT_SCHEDULE_RESERVATION_START_TIME, CONCERT_SCHEDULE_RESERVATION_END_TIME);
+
+            User savedUser1 = userService.joinUser(user1);
+            User savedUser2 = userService.joinUser(user2);
+            ConcertSchedule savedConcertSchedule = concertService.registerConcertSchedule(concertSchedule, 10000);
+
+            // 토큰 2개 발급 / 활성 상태
+            Token token1 = queueService.issueToken(savedUser1.getId(), savedConcertSchedule.getId());
+            Token token2 = queueService.issueToken(savedUser2.getId(), savedConcertSchedule.getId());
+            queueService.activateTokens(savedConcertSchedule.getId(), 2);
 
             // when : K개의 대기열 토큰 상태를 '만료'로 변경
+            queueService.expireToken(savedConcertSchedule.getId(), List.of(token1, token2));
 
             // then : K개의 대기열 토큰 상태가 '만료'로 변경된다.
-        }
+            Assertions.assertThatThrownBy(
+                    () -> queueService.validateToken(savedConcertSchedule.getId(), token1.getId())
+                    ).isInstanceOf(UnavailableRequestException.class)
+                    .hasMessageContaining("만료된 토큰이므로 사용 불가합니다.");
 
-        @Test
-        @DisplayName("성공 : 어떤 사용자의 대기열 토큰 상태가 '활성화'로 변경되면 서비스 진입에 성공한다.")
-        void shouldSuccessEnterService_WhenTokenStatusIsChangedToActive() {
-            // given : 어떤 사용자의 대기열 토큰이 '활성화' 상태로 변경된 경우
-
-            // when & then : 토큰 검증을 호출하고, 검증 결과는 성공이다.
-        }
-
-        @Test
-        @DisplayName("실패 : 어떤 사용자의 대기열 토큰 상태가 '비활성화'인 경우 서비스 진입에 실패한다.")
-        void shouldFailEnterService_WhenTokenStatusIsInactive() {
-            // given : 어떤 사용자의 대기열 토큰이 '비활성화' 상태
-
-            // when & then : 토큰 검증을 호출 시 실패 결과를 반환한다.
+            Assertions.assertThatThrownBy(
+                    () -> queueService.validateToken(savedConcertSchedule.getId(), token2.getId())
+                    ).isInstanceOf(UnavailableRequestException.class)
+                    .hasMessageContaining("만료된 토큰이므로 사용 불가합니다.");
         }
 
         @Test
         @DisplayName("성공 : 어떤 사용자의 대기열 순번을 조회할 수 있다.")
         void shouldSuccessGetQueueNumber_WhenGetQueueNumber() {
-            // given : 어떤 사용자의 대기열 토큰이 존재하고, '대기' 상태. 앞에 10명의 사용자가 대기 중.
+            // given : 어떤 사용자의 대기열 토큰이 존재하고, '대기' 상태. 앞에 10명의 사용자가 대기 중. (총 11명)
+            List<User> users = List.of(
+                    User.create("user1"),
+                    User.create("user2"),
+                    User.create("user3"),
+                    User.create("user4"),
+                    User.create("user5"),
+                    User.create("user6"),
+                    User.create("user7"),
+                    User.create("user8"),
+                    User.create("user9"),
+                    User.create("user10"),
+                    User.create("user11")
+            );
 
-            // when : 해당 사용자의 대기열 순번을 조회한다.
+            ConcertSchedule concertSchedule
+                    = ConcertSchedule.create(CONCERT_SCHEDULE_ID, CONCERT_SCHEDULE_START_TIME, CONCERT_SCHEDULE_RESERVATION_START_TIME, CONCERT_SCHEDULE_RESERVATION_END_TIME);
 
-            // then : 해당 사용자의 대기열 순번이 11이다.
-        }
+            ConcertSchedule savedConcertSchedule = concertService.registerConcertSchedule(concertSchedule, 10000);
 
-        @Test
-        @DisplayName("성공 : 현재 활성화된 대기열 토큰의 수를 조회할 수 있다.")
-        void shouldSuccessGetNumberOfActiveTokens_WhenGetNumberOfActiveTokens() {
-            // given : 현재 활성화된 대기열 토큰이 10개 존재한다.
+            // 토큰 11개 발급 / 대기 상태
+            ArrayDeque<Token> tokens = new ArrayDeque<>();
 
-            // when : 현재 활성화된 대기열 토큰의 수를 조회한다.
+            for(User user : users) {
+                userService.joinUser(user);
+                Token token = queueService.issueToken(user.getId(), savedConcertSchedule.getId());
+                tokens.addLast(token);
+            }
 
-            // then : 현재 활성화된 대기열 토큰의 수는 10이다.
+            // when : 목표 사용자의 대기열 순번을 조회한다.
+            Token targetToken = tokens.peekLast();
+            int queueNumber = queueService.getRemainingTokenCount(savedConcertSchedule.getId(), targetToken.getId());
+
+            // then : 해당 사용자의 대기열 순번이 10이다.
+            Assertions.assertThat(queueNumber).isEqualTo(10);
         }
     }
 
     @Nested
     class ConcurrentScenario {
-        @Test
-        @DisplayName("성공 : 동시에 100명 대기열 진입 시 100개의 대기열 토큰 발급에 성공한다.")
-        void shouldSuccessIssuing100Tokens_When100PeopleEnterQueue() {
-            // given : 스레드 100개 초기화. 각 스레드는 대기열 진입 시도.
-
-            // when : 해당 스레드 invokeAll() 호출 통해 동시에 토큰 발급 시도.
-
-            // then : 100개의 대기열 토큰이 발급된다. 대기 중 토큰 수는 100개.
-        }
 
         @Test
-        @DisplayName("성공 : 9명이 순서대로 대기열에 진입한다. 이때 매번 마지막 사용자의 순번을 조회하는데, 이 순번은 1씩 감소한다.")
-        void shouldSuccessGetQueueNumber_When9PeopleEnterQueueConcurrently() {
+        @DisplayName("성공 : 10명이 동시에 대기열에 진입한다. 10개 모두 성공한다.")
+        void shouldSuccessGetQueueNumber_When9PeopleEnterQueueConcurrently()
+                throws InterruptedException, ExecutionException {
             // given
+            // 사용자 10명 초기화
+            List<User> users = List.of(
+                    User.create("user1"),
+                    User.create("user2"),
+                    User.create("user3"),
+                    User.create("user4"),
+                    User.create("user5"),
+                    User.create("user6"),
+                    User.create("user7"),
+                    User.create("user8"),
+                    User.create("user9"),
+                    User.create("user10")
+            );
+            // 공연 일정 초기화
+            ConcertSchedule concertSchedule
+                    = ConcertSchedule.create(CONCERT_SCHEDULE_ID, CONCERT_SCHEDULE_START_TIME, CONCERT_SCHEDULE_RESERVATION_START_TIME, CONCERT_SCHEDULE_RESERVATION_END_TIME);
 
+            // 사용자 및 공연 일정 저장
+            ConcertSchedule savedConcertSchedule = concertService.registerConcertSchedule(concertSchedule, 10000);
+            users.forEach(user -> userService.joinUser(user));
+
+            // 토큰 발급 요청 10개 동시 발생
+            ExecutorService executorService = Executors.newFixedThreadPool(10);
+            List<Callable<Boolean>> tasks = new ArrayList<>();
+            for(User user : users) {
+                tasks.add(() -> {
+                    try {
+                        Token token = queueService.issueToken(user.getId(), savedConcertSchedule.getId());
+                        return true;
+                    }
+                    catch (Exception e) {
+                        return false;
+                    }
+                });
+            }
             // when
+            Collections.shuffle(tasks);
+            List<Future<Boolean>> results = executorService.invokeAll(tasks);
+            executorService.shutdown();
 
+            int successCount = 0;
+            for(Future<Boolean> result : results) {
+                if(result.get()) {
+                    successCount++;
+                }
+            }
             // then
+            Assertions.assertThat(successCount).isEqualTo(10);
+
         }
     }
 }
