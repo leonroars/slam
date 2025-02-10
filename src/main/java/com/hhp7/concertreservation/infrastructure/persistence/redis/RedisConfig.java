898ba94ec73cf157126269639248cdcd4afe5336
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2025-02-10 16:12:38 +0900

    feat: RedisConfig 수정 및 통합테스트 수정

--- src/main/java/com/hhp7/concertreservation/infrastructure/persistence/redis/RedisConfig.java
+++ src/main/java/com/hhp7/concertreservation/infrastructure/persistence/redis/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.hhp7.concertreservation.infrastructure.persistence.redis;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
@@ -9,12 +10,10 @@ import com.hhp7.concertreservation.domain.queue.model.Token;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -30,7 +29,6 @@ public class RedisConfig {
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisHost, redisPort);
     }
-
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -67,6 +65,7 @@ public class RedisConfig {
         // Java 8 LocalDataTime 직렬화를 위한 ObjectMapper 설정. 이 부분 필수!
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES); // Java
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.activateDefaultTyping(
                 LaissezFaireSubTypeValidator.instance,
