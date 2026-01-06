fa9a791863465e65783da91e033e34850de9e0cd
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2026-01-07 00:38:39 +0900

    <feat> 예약 생성 API 멱등성 보장 설계 : Idempotent Annotation Interface 구현

--- src/main/java/com/slam/concertreservation/component/idempotency/Idempotent.java
+++ src/main/java/com/slam/concertreservation/component/idempotency/Idempotent.java
@@ -1,4 +1,25 @@
 package com.slam.concertreservation.component.idempotency;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Idempotent {
+
+    /**
+     * 메서드 매개변수나 기타 컨텍스트에서 Idempotency Key 를 동적으로 생성하는 데 사용됩니다.
+     */
+    String key();
+
+    /**
+     * 응답 캐시 지속 시간 단위 및 지속 시간 정의
+     * @return
+     */
+    TimeUnit responseCacheTimeUnit() default TimeUnit.HOURS;
+    long responseCacheDuration() default 24;
+
 }
