2df7c5fcff6ff656981d10f2c53b375c867e04ea
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2026-01-07 00:08:13 +0900

    <chore> IdempotencyRepository 명세 변경 : SETNX Lock TTL을 Physical Time 기준으로 설정했을 때, 만료 시 발생가능한 Race Condition 방지 위해 Redisson Lock 으로 해당 책임 이관 / 따라서 삭제

--- src/main/java/com/slam/concertreservation/component/idempotency/IdempotencyRepository.java
+++ src/main/java/com/slam/concertreservation/component/idempotency/IdempotencyRepository.java
@@ -23,27 +23,14 @@ public class IdempotencyRepository {
                 .set(idempotencyKey, record, ttl);
     }
 
+    /**
+     * idempotencyKey 에 해당하는 IdempotencyRecord 를 Redis 에서 조회합니다.
+     * @param idempotencyKey
+     * @return
+     */
     public Optional<IdempotencyRecord> findByIdempotencyKey(String idempotencyKey) {
         IdempotencyRecord record = idempotencyRecordRedisTemplate.opsForValue()
                 .get(idempotencyKey);
         return Optional.ofNullable(record);
-    }
-
-    /**
-     * idempotencyKey 가 존재하지 않을 때에만 IdempotencyRecord 를 Redis 에 저장합니다.
-     * <br></br>
-     * 목적 : 복수의 요청이 동시에 혹은 거의 동시에 애플리케이션에 진입했을 때, 최초 요청에 대해서만 처리하고 이후 요청에 대해서는 무시하여 경합 수준을 낮추기 위함.
-     * <br></br>
-     * 작동 방식 : Redis 의 SET...NX EX {ttl} 과 동일하게 작동.
-     * <br></br>
-     * @param idempotencyKey
-     * @param record
-     * @param ttl
-     * @return
-     */
-    public boolean saveIfAbsent(String idempotencyKey, IdempotencyRecord record, Duration ttl) {
-        Boolean success = idempotencyRecordRedisTemplate.opsForValue()
-                .setIfAbsent(idempotencyKey, record, ttl);
-        return Boolean.TRUE.equals(success);
     }
 }
