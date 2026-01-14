c8fddfd67b65033e1cb6f045b4adb1298c5254a9
Ho Yeon CHAE <comfynumb@hufs.ac.kr>
2026-01-14 14:56:27 +0900

    <chore> IdempotencyRecord 필드 수정 : HttpStatus -> HttpStatusCode

--- src/main/java/com/slam/concertreservation/component/idempotency/IdempotencyRecord.java
+++ src/main/java/com/slam/concertreservation/component/idempotency/IdempotencyRecord.java
@@ -4,12 +4,13 @@ import java.util.Map;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Builder
 @Data
 public class IdempotencyRecord {
 
-    HttpStatus httpStatus; // 캐시된 응답의 HTTP 응답 코드
+    HttpStatusCode httpStatusCode; // 캐시된 응답의 HTTP 응답 코드
     Map<String, String> headers; // 캐시된 응답의 HTTP 헤더
     String body; // ResponseEntity 의 Body 를 구성하는 DTO 의 직렬화된 형태
     String bodyType; // ObjectMapper 를 활용해 Body 역직렬화 시 참고할 클래스 식별자
